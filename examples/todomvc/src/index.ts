7c9c663c4e57a0098607a67faa5a9ee40ccdd3bc
Vladislav Lagunov <lagunov.v@bitmaster.ru>
2019-03-26 18:46:35 +0400

    Adopt purescript-incremental-functions

--- examples/todomvc/src/index.ts
+++ examples/todomvc/src/index.ts
@@ -1,11 +1,12 @@
-import { h, Patch, array, create, zoom, actuate, applyPatch, noop, RawPatch, preparePatch, nodeIndex } from '../../../src';
+import { h, Patch, array, create, actuate, applyPatch, noop, RawPatch, preparePatch, nodeIndex, Jet } from '../../../src';
+import * as I from '../../../src/incremental';
 import * as todo from './todo';
 import '../node_modules/todomvc-app-css/index.css';
 
 // Model
 export type Model = {
   title: string;
-  todos: todo.Model[];
+  todos: ReturnType<typeof todo.init>[];
   filter: Filter;
 };
 
@@ -15,7 +16,6 @@ export type Filter = 'all'|'active'|'completed';
 // Action
 export type Action =
   | { tag: 'Edit', value: string }
-  | { tag: 'Filter/set', filter: Filter }
   | { tag: 'ToggleAll' }
   | { tag: 'ClearCompleted' }
   | { tag: 'KeyDown', event: KeyboardEvent }
@@ -26,12 +26,13 @@ export type Action =
 export function update(action: Action, model: Model): RawPatch<Model> {
   switch (action.tag) {
     case 'Edit': return { $patch: { title: action.value } };
-    case 'Filter/set': return [];
     case 'ToggleAll': {
       const checked = allChecked(model);
       return model.todos.map((_, idx) => ({ $at: ['todos', idx], patch: { $patch: { completed: !checked } } }));
     }
-    case 'ClearCompleted': return [];
+    case 'ClearCompleted': {
+      return { $at: 'todos', patch: { $batch: model.todos.reduceRight((acc, todo, idx) => (todo.completed && acc.push({ $remove: idx }), acc), [] as any) } };
+    }
     case 'KeyDown': {
       if (action.event.keyCode === KEY_ENTER && model.title) return [
         { $patch: { title: '' } },
@@ -39,7 +40,11 @@ export function update(action: Action, model: Model): RawPatch<Model> {
       ];
       return [];
     }
-    case 'HashChange': return [];
+    case 'HashChange': {
+      if (action.hash === '#/completed') return { $patch: { filter: 'completed' } };
+      if (action.hash === '#/active') return { $patch: { filter: 'active' } };
+      return { $patch: { filter: 'all' } };
+    }
     case '@Todo': {
       if (action.action.tag === 'Destroy') return { $at: 'todos', patch: { $remove: action.idx } };
       if (action.action.tag === 'Editing/commit' && model.todos[action.idx].editing === '') return { $at: 'todos', patch: { $remove: action.idx } };
@@ -64,20 +69,21 @@ export const view = h.div<Model, Action>(
         click: () => ({ tag: 'ToggleAll' }),
       }),
       h.label('Mark all as complete').attrs({ for: 'toggle-all' }),
-      h.ul({ class: 'todo-list' }).childs(
-        array<Model, Action, 'todos'>(
-          'todos',
-          todo.view.dimap(zoom('item'), (action, model, el) => ({ tag: '@Todo', action, idx: nodeIndex(el) } as Action))
-        ),
+      // @ts-ignore
+      array<Model, Action, 'todos'>(
+        'todos', 'ul', { class: 'todo-list' },
+        todo.view.dimap(projectItem, (action, model, el) => ({ tag: '@Todo', action, idx: nodeIndex(el) } as Action)),
       ),
       h.footer({ class: 'footer' }).childs(
-        h.span({ class: 'todo-count'}).childs(h('string', countItemsLeft, ' items left')),
+        h.span({ class: 'todo-count'}).childs(h('strong', countItemsLeft), ' items left'),
         h.ul({ class: 'filters' }).childs(
           h.li(h.a('All').attrs({ href: '#/', class: (m: Model) => m.filter === 'all' ? 'selected' : '' })),
           h.li(h.a('Active').attrs({ href: '#/active', class: (m: Model) => m.filter === 'active' ? 'selected' : '' })),
           h.li(h.a('Completed').attrs({ href: '#/completed', class: (m: Model) => m.filter === 'completed' ? 'selected' : '' })),
         ),
-        h.button(`Clear completed`).attrs({ class: "clear-completed" }),
+        h.button(`Clear completed`).attrs({ class: "clear-completed" }).on({
+          click: () => ({ tag: 'ClearCompleted' })
+        }),
       ),
     ),
   ),
@@ -85,7 +91,7 @@ export const view = h.div<Model, Action>(
   h.footer({ class: 'info' }).childs(
     h.p('Double-click to edit a todo'),
     h.p('Template by ', h.a('Sindre Sorhus').attrs({ href: "http://sindresorhus.com" })),
-    h.p('Created by ', h.a('Lagunov Vlad').attrs({ href: "http://todomvc.com" })),
+    h.p('Created by ', h.a('Lagunov Vlad').attrs({ href: "https://github.com/lagunoff" })),
     h.p('Part of ', h.a('TodoMVC').attrs({ href: "http://todomvc.com" })),
   ),
 );
@@ -98,22 +104,42 @@ function allChecked(model: Model): boolean {
   return model.todos.reduce((acc, todo) => todo.completed ? (acc && true) : false, true);
 }
 
+function projectItem(m: Jet<{ item: Model['todos'][number], model: Model }>) {
+  const filter = m.key('model', 'filter');
+  const isCompleted = I.if_then_else(m.key('item', 'completed'), Jet.of('completed'), Jet.of('active'));
+  const visible = I.if_then_else(
+    I.eq(filter, Jet.of('all')),
+    Jet.of(true),
+    I.eq(filter, isCompleted),
+  );
+                                     
+  return I.merge(m.key('item'), I.record({
+    hidden: I.neg(visible),
+  }));
+}
+ 
+function handleAction(action: Action) {
+  const rawPatch = update(action, model);
+  const patch = preparePatch(model, rawPatch);
+  actuate(el, sdom, new Jet(model, patch));
+  model = applyPatch(model, patch, true);
+  console.log('action', action);
+  console.log('rawPatch', rawPatch);
+  console.log('patch', patch);
+  console.log('model', model);
+  console.log('-----------');
+}
+
 
 let model: Model = { filter: 'all', todos: [], title: '' };
 const container = document.createElement('div');
 document.body.appendChild(container);
 const getModel = () => model;
-const sdom = view.map((action) => {
-  const rawPatch = update(action, model);
-  const patch = preparePatch(model, rawPatch);
-  model = applyPatch(model, patch);
-  console.log('action', action);
-  console.log('rawPatch', rawPatch);
-  console.log('patch', patch);
-  console.log('model', model);
-  actuate(el, sdom, () => model, patch);
-})
+const sdom = view.map(handleAction)
 const el = create(sdom, getModel);
+window.onpopstate = function(event) {
+  handleAction({ tag: 'HashChange', hash: location.hash });
+};
 container.appendChild(el);
 
 const KEY_ENTER = 13;
